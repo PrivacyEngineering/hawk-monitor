2bcea3bdad5be36e3e24d4a0cd93923e4e7d1927
Piotrek Witkowski <witkowski@campus.tu-berlin.de>
2021-12-14 00:37:47 +0100

    Add Navigation

--- frontend-v2/src/App.tsx
+++ frontend-v2/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './HomePage';
+import { MappingsPage } from './MappingsPage';
+import { Navigation } from './Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="mappings" element={<MappingsPage />}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
---
+++ frontend-v2/src/MappingsPage.tsx
@@ -0,0 +1,5 @@
+export const MappingsPage = () => {
+  return (
+    <>Mappings Page</>
+  )
+}
