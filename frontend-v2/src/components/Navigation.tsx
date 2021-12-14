7cef9d73be688c7f4620f3ebb75cdccf98f2acc8
Piotrek Witkowski <witkowski@campus.tu-berlin.de>
2021-12-14 22:21:27 +0100

    Bring branding back to navigation

--- frontend-v2/src/components/Navigation.tsx
+++ frontend-v2/src/components/Navigation.tsx
@@ -8,11 +8,11 @@ export const Navigation = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        {/* <Navbar.Brand href="/">Transparency Dashboard</Navbar.Brand> */}
+        <Navbar.Brand>Transparency Dashboard</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" active={location.pathname === '/'}>Transparency Dashboard</Nav.Link>
+            <Nav.Link as={Link} to="/" active={location.pathname === '/'}>Home</Nav.Link>
             <Nav.Link as={Link} to="/settings" active={location.pathname === '/settings'}>Settings</Nav.Link>
           </Nav>
         </Navbar.Collapse>
