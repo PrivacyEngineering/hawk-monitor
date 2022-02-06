be9c2c693916ef667939904af4c6746457ff50d5
Piotrek Witkowski <piwit@amazon.de>
2022-02-06 14:24:51 +0100

    Cleanup Admin.js

--- frontend-v3-purity-ui/src/layouts/Admin.js
+++ frontend-v3-purity-ui/src/layouts/Admin.js
@@ -20,8 +20,6 @@ export default function Dashboard(props) {
   // states and functions
   const [sidebarVariant, setSidebarVariant] = useState("transparent");
   const [fixed, setFixed] = useState(false);
-  // ref for main panel div
-  const mainPanel = React.createRef();
   // functions for changing the states from components
   const getRoute = () => {
     return window.location.pathname !== "/admin/full-screen-maps";
