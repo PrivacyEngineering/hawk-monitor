3154e43e694a134edaca2ba5d2272abe32739252
Piotrek Witkowski <piwit@amazon.de>
2022-02-06 14:13:52 +0100

    Clean up routes

--- frontend-v3-purity-ui/src/routes.js
+++ frontend-v3-purity-ui/src/routes.js
@@ -1,14 +1,8 @@
 import Dashboard from "views/Dashboard/Dashboard.js";
 import Tables from "views/Dashboard/Tables.js";
-import Billing from "views/Dashboard/Billing.js";
-import Profile from "views/Dashboard/Profile.js";
-import SignIn from "views/Pages/SignIn.js";
-import SignUp from "views/Pages/SignUp.js";
 
 import {
   HomeIcon,
-  StatsIcon,
-  CreditIcon,
   DocumentIcon,
 } from "components/Icons/Icons";
 
@@ -16,22 +10,15 @@ var dashRoutes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    icon: <HomeIcon color="inherit" />,
+    icon: <HomeIcon />,
     component: Dashboard,
     layout: "/admin",
   },
   {
     path: "/tables",
     name: "Tables",
-    icon: <StatsIcon color="inherit" />,
+    icon: <DocumentIcon />,
     component: Tables,
-    layout: "/admin",
-  },
-  {
-    path: "/billing",
-    name: "Billing",
-    icon: <CreditIcon color="inherit" />,
-    component: Billing,
     layout: "/admin",
   },
 ];
