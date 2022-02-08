dd15061bb87b340c000165c8d1e29874c274a705
Piotrek Witkowski <piwit@amazon.de>
2022-02-08 12:57:08 +0100

    Fix SidebarResponsive

--- frontend-v3-purity-ui/src/components/Navbars/AdminNavbarLinks.js
+++ frontend-v3-purity-ui/src/components/Navbars/AdminNavbarLinks.js
@@ -1,58 +1,41 @@
-// Chakra Icons
 import { BellIcon } from "@chakra-ui/icons";
-// Chakra Imports
 import { Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text, useColorModeValue } from "@chakra-ui/react";
-// Assets
 import avatar1 from "assets/img/avatars/avatar1.png";
 import avatar2 from "assets/img/avatars/avatar2.png";
 import avatar3 from "assets/img/avatars/avatar3.png";
-// Custom Icons
 import { ProfileIcon, SettingsIcon } from "components/Icons/Icons";
-// Custom Components
 import { ItemContent } from "components/Menu/ItemContent";
 import { SidebarResponsive } from "components/Sidebar/SidebarResponsive";
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import routes from "routes.js";
 
 export default function HeaderLinks(props) {
-  const { variant, children, fixed, secondary, onOpen } = props;
+  const { onOpen, secondary } = props;
 
-  let mainText = useColorModeValue("gray.700", "gray.200");
-  let navbarIcon = useColorModeValue("gray.500", "gray.200");
+  const [navbarIconColor, setNavbarIconColor] = useState('');
+  const [mainTextColor, setMainTextColor] = useState('');
 
-  if (secondary) {
-    navbarIcon = "white";
-    mainText = "white";
-  }
+  const mainTextColorDefault = useColorModeValue("gray.700", "gray.200");
+  const navbarIconColorDefault = useColorModeValue("gray.500", "gray.200");
+
+  useState(() => {
+    setNavbarIconColor(secondary ? "white" : navbarIconColorDefault);
+    setMainTextColor(secondary ? "white" : mainTextColorDefault);
+  })
+  
   return (
-    <Flex
-      pe={{ sm: "0px", md: "16px" }}
-      w={{ sm: "100%", md: "auto" }}
-      alignItems="center"
-      flexDirection="row"
-    >
+    <Flex pe={{ sm: "0px", md: "16px" }} w={{ sm: "100%", md: "auto" }} alignItems="center" flexDirection="row">
       <NavLink to="#">
-        <Button
-          ms="0px"
-          px="0px"
-          me={{ sm: "2px", md: "16px" }}
-          color={navbarIcon}
-          variant="transparent-with-icon"
-          leftIcon={<ProfileIcon color={navbarIcon} w="22px" h="22px" me="0px" />}
-        >
+        <Button ms="0px" px="0px" me={{ sm: "2px", md: "16px" }} color={mainTextColor} variant="transparent-with-icon" leftIcon={<ProfileIcon color={navbarIconColor} w="22px" h="22px" me="0px" />}>
           <Text display={{ sm: "none", md: "flex" }}>Sign Out</Text>
         </Button>
       </NavLink>
-      <SidebarResponsive
-        logoText={props.logoText}
-        secondary={props.secondary}
-        routes={routes}
-      />
-      <SettingsIcon cursor="pointer" ms={{ base: "16px", xl: "0px" }} me="16px" onClick={props.onOpen} color={navbarIcon} w="18px" h="18px" />
+      <SidebarResponsive />
+      <SettingsIcon cursor="pointer" ms={{ base: "16px", xl: "0px" }} me="16px" onClick={onOpen} color={navbarIconColor} w="18px" h="18px" />
       <Menu>
         <MenuButton>
-          <BellIcon color={navbarIcon} w="18px" h="18px" />
+          <BellIcon color={navbarIconColor} w="18px" h="18px" />
         </MenuButton>
         <MenuList p="16px 8px">
           <Flex flexDirection="column">
